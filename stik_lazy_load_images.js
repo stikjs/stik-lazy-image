d87789f98ba3b53c21fe147ec5145c6b2189b337
Lukas Alexandre <lukas@codelogic.me>
2014-04-12 19:29:30 +0200

    Refactoring code to improve readability

--- stik_lazy_load_images.js
+++ stik_lazy_load_images.js
@@ -1,60 +1,67 @@
-(function() {
-  stik.boundary({
-    as: 'global',
-    to: window
-  });
+stik.boundary({
+  as: 'lazyPicture',
+  from: 'behavior',
+  resolvable: true,
+  to: function($window, $h){
+    return function(elm, opts){
+      var obj = {},
+          offset,
+          throttle;
 
-  stik.boundary({
-    as: 'lazyPicture',
-    from: 'behavior',
-    resolvable: true,
-    to: function(global) {
-      return function(elm, opts) {
-        var defineListeners, inView, obj, offset, removeListeners, throttle, _throttle;
-        if (opts == null) {
-          opts = {};
+      if (opts == null) { opts = {}; }
+
+      offset = parseInt(opts.offset || 0);
+      throttle = parseInt(opts.throttle || 250);
+
+      function defineListeners(){
+        if (document.addEventListener) {
+          $window.addEventListener('scroll', $h.debounce(_throttle, 100), false);
+          return $window.addEventListener('load', _throttle, false);
+        } else {
+          $window.attachEvent('onscroll', $h.debounce(_throttle, 100));
+          return $window.attachEvent('onload', _throttle);
         }
-        obj = {};
-        offset = parseInt(opts.offset || 0);
-        throttle = parseInt(opts.throttle || 250);
-        defineListeners = function() {
-          if (document.addEventListener) {
-            global.addEventListener('scroll', _throttle, false);
-            return global.addEventListener('load', _throttle, false);
-          } else {
-            global.attachEvent('onscroll', _throttle);
-            return global.attachEvent('onload', _throttle);
-          }
-        };
-        removeListeners = function() {
-          if (document.removeEventListener) {
-            return global.removeEventListener('scroll', _throttle);
-          } else {
-            return global.detachEvent('onscroll', _throttle);
-          }
-        };
-        _throttle = function() {
-          var poll;
-          clearTimeout(poll);
-          return poll = setTimeout(obj.loadImage, throttle);
-        };
-        inView = function() {
-          var coords;
-          coords = elm.getBoundingClientRect();
-          return (coords.top >= 0 && coords.left >= 0 && coords.top) <= (window.innerHeight || document.documentElement.clientHeight) + offset;
-        };
-        obj.loadImage = function() {
-          if (inView() && elm.hasAttribute('data-src')) {
-            elm.style.background = 'url(' + elm.getAttribute('data-src') + ')';
-            elm.removeAttribute('data-src');
-            return removeListeners();
-          }
-        };
-        defineListeners();
-        obj.loadImage();
-        return obj;
-      };
-    }
-  });
+      }
 
-}).call(this);
+      function removeListeners(){
+        if (document.removeEventListener) {
+          return $window.removeEventListener('scroll', _throttle);
+        } else {
+          return $window.detachEvent('onscroll', _throttle);
+        }
+      }
+
+      function _throttle(){
+        var poll;
+        clearTimeout(poll);
+        return poll = setTimeout(obj.loadImage, throttle);
+      }
+
+      function inView(){
+        return distanceFromTop() <= consideredArea();
+      }
+
+      function distanceFromTop(){
+        var coords = elm.getBoundingClientRect();
+        return coords.top >= 0 && coords.left >= 0 && coords.top;
+      }
+
+      function consideredArea(){
+        return ($window.innerHeight || document.documentElement.clientHeight) + offset;
+      }
+
+      obj.loadImage = function loadImage(){
+        if (inView() && elm.hasAttribute('data-src')) {
+          elm.style.background = 'url(' + elm.getAttribute('data-src') + ')';
+          elm.removeAttribute('data-src');
+          return removeListeners();
+        }
+      };
+
+      defineListeners();
+      obj.loadImage();
+
+      return obj;
+    };
+  }
+});
